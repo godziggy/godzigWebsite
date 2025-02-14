716f44a1a7429070c01beca3806fcf1fbcfe5df0
Tate Frost <tatefrost96@gmail.com>
2025-02-13 21:07:28 -0700

    initialcommit

---
+++ script.js
@@ -0,0 +1,23 @@
+function flashRandomNumber() {
+
+  const elementIds = ["element1", "element2", "element3"];
+
+  for (let id of elementIds) {
+    let randomNumber = Math.floor(Math.random() * 8) + 1;
+
+    let element = document.getElementById(id).textContent = randomNumber;
+
+    setTimeout(flashRandomNumber, 1000);
+  }
+
+  // let randomNumber = Math.floor(Math.random() * 8) + 1; // Generate random number between 1 and 100
+
+  // document.getElementById("random-number").textContent = randomNumber;
+
+  // setTimeout(flashRandomNumber, 1000); // Update every 100 milliseconds
+
+}
+
+
+ // Start flashing on page load
+ flashRandomNumber();
